9be366089a2bb04b0ef3b11741b3be1d1f961950
Sugith K <134681105+sugith10@users.noreply.github.com>
2024-07-14 23:54:00 +0530

    fix: Fixed localhost link issue

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 const start = () => {
 
     httpServer.listen(PORT, () => {
-    console.log(`server has been connected on http://localhost/${PORT}`);
+    console.log(`server has been connected on http://localhost:${PORT}`);
     connectDB(); 
   });
 };
